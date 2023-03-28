b4bd462a5cc02a0ce57cc9eac9a618f81bca9e4f
lfurmanchuk <lesyazhuruk@gmail.com>
2023-03-28 15:04:02 +0300

    hooks refactoring App.jsx, Form.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,119 +1,70 @@
-import React, { Component } from 'react';
-import { Form } from './Form/Form';
+// import React, { Component } from 'react';
+
+import { useState, useEffect } from 'react';
+import Form from './Form/Form';
 import { nanoid } from 'nanoid';
 import { Section } from './Section/Section';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './FormList/FormList';
 import { SectionTitle } from './Section/Section.styled';
 
-export class App extends Component {
-  // Стейт з базовими даними відповідно до завдання
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    // Початкове заповнення поля Find contacts by name
-    filter: '',
+export default function App() {
+  const [filter, setFilter] = useState('');
+  const [contacts, setСontacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) || []
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const formSubmitHandler = (name, number) => {
+    contacts.some(contact => contact.name === name)
+      ? alert(`${name} is already in contacts`)
+      : setСontacts([
+          ...contacts,
+          {
+            id: nanoid(),
+            name: name,
+            number: number,
+          },
+        ]);
   };
 
-  // componentDidMount() викликається відразу після монтування компонента
-  //  не можна робити стрілковою функцією!
-  componentDidMount() {
-    // зчитує сховище, парсить його у масив і запусує у state
-    const contacts = localStorage.getItem('contactsList');
-    const parsedContacts = JSON.parse(contacts);
-
-    // перевіряє навність даних у сховищі і при навності записує у state
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  // componentDidUpdate() викликається відразу після оновлення
-  // не викликається під час першого рендеру
-  //  не можна робити стрілковою функцією!
-  componentDidUpdate(prevProps, prevState) {
-    // порівнює поточні пропси з попередніми
-    if (this.state.contacts !== prevState.contacts)
-      localStorage.setItem('contactsList', JSON.stringify(this.state.contacts));
-  }
-
-  addContact = ({ name, number }) => {
-    // Перевірка на дублювання
-    if (this.checkContact(name)) {
-      alert(`${name} is already in contacts`);
-      return;
-    }
-
-    const contact = {
-      name,
-      id: nanoid(),
-      number,
-    };
-
-    this.setState(prevState => ({
-      contacts: [contact, ...prevState.contacts],
-    }));
+  const handleChange = evt => {
+    setFilter(evt.target.value);
   };
 
-  // перевірка контакту у списку
-  checkContact = checkedNameContact => {
-    const res = this.state.contacts.find(
-      contact => contact.name === checkedNameContact
-    );
-    return res;
+  const handleDelete = id => {
+    setСontacts(contacts.filter(contact => contact.id !== id));
   };
 
-  // Метод, що спостерігає за полем фільтрації і пише в стейт
-  changeFilter = event => {
-    this.setState({ filter: event.currentTarget.value });
-  };
+  const contactsFiltered = [];
+  contacts.forEach(contact => {
+    contact.name.toLowerCase().includes(filter.toLowerCase()) &&
+      contactsFiltered.push(contact);
+  });
 
-  // Метод для фільтрування контактів по введеним у полі пошука і повернення результату фільтру
-  filterContacts = () => {
-    const { contacts, filter } = this.state;
-
-    const normalizedFilter = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
-  // Метод для видалення контакту
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
-  };
-
-  render() {
-    const { filter } = this.state;
-    const filteredResults = this.filterContacts();
-    return (
-      <div
-        style={{
-          // height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 20,
-        }}
-      >
-        <Section title="Phonebook">
-          <Form onFormSubmit={this.addContact}></Form>
-          <SectionTitle>Contacts</SectionTitle>
-          <Filter value={filter} onChange={this.changeFilter} />
-          <ContactList
-            contacts={filteredResults}
-            onDeleteContact={this.deleteContact}
-          />
-        </Section>
-      </div>
-    );
-  }
+  return (
+    <div
+      style={{
+        // height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+      }}
+    >
+      <Section title="Phonebook">
+        <Form onFormSubmit={formSubmitHandler}></Form>
+        <SectionTitle>Contacts</SectionTitle>
+        <Filter value={filter} onChange={handleChange} />
+        <ContactList
+          contacts={contactsFiltered}
+          onDeleteContact={handleDelete}
+        />
+      </Section>
+    </div>
+  );
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,91 +1,80 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import { Label, Input, AddBtn, FormField } from './Form.styled';
 
-export class Form extends Component {
-  // PropTypes як статична властивысть
-  static propTypes = {
-    onFormSubmit: PropTypes.func.isRequired,
-  };
-
-  // Стейт форми з початковим пустим значенням
-  state = {
-    name: '',
-    number: '',
-  };
+export default function Form({ formSubmitHandler }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
   // змінна ID
-  nameInputId = nanoid();
-  telInputId = nanoid();
+  // nameInputId = nanoid();
+  // telInputId = nanoid();
 
   // Метод, що спостерігає за інпутами і записує в state їх значення
-  handleChange = e => {
+  const handleChange = e => {
     const { name, value } = e.currentTarget;
 
-    this.setState({ [name]: value });
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        break;
+    }
   };
 
   // Метод на відправці форми, що формує зі state контакт і передає до зовнішного методу
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    const contact = {
-      id: nanoid(),
-      name: this.state.name,
-      number: this.state.number,
-    };
-
-    // Зовнішній метод в пропсах класу
-    this.props.onFormSubmit(contact);
-
-    this.reset();
+    formSubmitHandler(name, number);
+    setName('');
+    setNumber('');
   };
 
-  // Очищення полів форми після відправки
-  reset = () => {
-    this.setState({ name: '', number: '', id: '' });
-  };
-
-  render() {
-    return (
-      <form autoComplete="off" onSubmit={this.handleSubmit}>
-        <FormField>
-          <Label htmlFor="name" id={this.nameInputId}>
-            Name
-          </Label>
-          <Input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            // Спостерігаючий метод
-            onChange={this.handleChange}
-            id={this.nameInputId}
-            // Пишемо значення до state
-            value={this.state.name}
-          />
-        </FormField>
-        <FormField>
-          <Label htmlFor="tel" id={this.telInputId}>
-            Number
-          </Label>
-          <Input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            id={this.telInputId}
-            // Пишемо значення до stat
-            value={this.state.number}
-            // Спостерігаючий метод
-            onChange={this.handleChange}
-          />
-        </FormField>
-        <AddBtn type="submit">Add contact</AddBtn>
-      </form>
-    );
-  }
+  return (
+    <form autoComplete="off" onSubmit={handleSubmit}>
+      <FormField>
+        <Label htmlFor="name">Name</Label>
+        <Input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          // Спостерігаючий метод
+          onChange={handleChange}
+          // id={nameInputId}
+          // Пишемо значення до state
+          value={name}
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="tel">Number</Label>
+        <Input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          // id={telInputId}
+          // Пишемо значення до stat
+          value={number}
+          // Спостерігаючий метод
+          onChange={handleChange}
+        />
+      </FormField>
+      <AddBtn type="submit">Add contact</AddBtn>
+    </form>
+  );
 }
+
+Form.propTypes = {
+  formSubmitHandler: PropTypes.func.isRequired,
+};
